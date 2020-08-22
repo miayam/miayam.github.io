504cfc7e54140131df379558b2c4e92bc81f0892
miayam <deni@99.co>
2020-08-22 16:14:13 +0700

    feat(init): Try out good starter project.

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+module.exports = function(eleventyConfig) {
+  return {
+    dir: {
+      input: "./",      // Equivalent to Jekyll's source property
+      output: "./_site" // Equivalent to Jekyll's destination property
+    }
+  };
+};
+
